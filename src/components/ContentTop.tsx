71b0ff31fa720ae61153a3908094ea763bfc602f
Uros Cosic <uros.cosic.1337@gmail.com>
2023-11-18 01:21:00 +0100

    bolder heading

--- src/components/ContentTop.tsx
+++ src/components/ContentTop.tsx
@@ -11,7 +11,7 @@ function ContentTop({ heading }: { heading: string }) {
         width={1634}
         height={610}
       />
-      <h1 className="absolute text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase font-bold text-5xl">
+      <h1 className="absolute text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase font-black text-5xl">
         {heading}
       </h1>
     </div>
