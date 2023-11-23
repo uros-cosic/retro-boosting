f31c3120c5250e91a9fb21c8d08241b29222c3bd
Uros Cosic <uros.cosic.1337@gmail.com>
2023-11-23 06:19:20 +0100

    skeletons2

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,16 +3,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function Loading() {
   return (
     <>
-      <Skeleton className="bg-muted-foreground w-full h-[60vh]" />
-      <div className="my-10 w-full h-24">
-        <div className="flex items-center justify-between mx-auto max-w-5xl">
-          <Skeleton className="bg-muted-foreground w-1/5 h-24" />
-          <Skeleton className="bg-muted-foreground w-1/5 h-24" />
-          <Skeleton className="bg-muted-foreground w-1/5 h-24" />
-          <Skeleton className="bg-muted-foreground w-1/5 h-24" />
-        </div>
-      </div>
-      <Skeleton className="bg-muted-foreground w-full h-[65vh]" />
+      <Skeleton className="bg-muted-foreground w-full h-[60vh] rounded-none" />
+      <div className="h-screen" />
     </>
   );
 }
