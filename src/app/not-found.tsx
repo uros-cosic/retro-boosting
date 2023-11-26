b3837f926e96e4a8b258c650d747ccb83bb02387
Uros Cosic <uros.cosic.1337@gmail.com>
2023-11-26 17:28:34 +0100

    404 page fix?

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ function NotFound() {
     <div className="py-10 h-[80vh] w-screen text-white flex flex-col items-center justify-center space-y-5">
       <h1 className="text-center text-3xl font-black">404 - Not Found</h1>
       <p className="text-lg text-gray-300 text-center">
-        Page that you are searching for was deleted or doesn't exist
+        {"Page that you are searching for was deleted or doesn't exist"}
       </p>
       <Link href="/" className="underline">
         Home Page
